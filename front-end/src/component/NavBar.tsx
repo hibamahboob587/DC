ef5473e1e3ad8c6097809d3a163980c2c7d3314f
hibamahboob587 <hibamahboob11@gmail.com>
2026-05-20 04:18:07 +0500

    add logout button to navbar, home.jpg background on all pages

--- front-end/src/component/NavBar.tsx
+++ front-end/src/component/NavBar.tsx
@@ -1,56 +1,50 @@
 import "../Style/NavBar.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
 import profileHeaderImg from "../assets/profile-header.jpg";
 
 function NavBar() {
-  const [activeLink, setActiveLink] = useState("home");
-  
-    const handleLinkClick = (name: string) => {
-      setActiveLink(name);
-    };
-    return (
-      <header className="header-area header-sticky">
-        <div className="container">
-          <div className="row">
-            <div className="col-12">
-              <nav className="main-nav">
-                <a href="/" className="logo">
-                  <h4>
-                    <em>Manga</em> Hub
-                  </h4>
-                </a>
-                <div className="search-input">
-                </div>
-                <ul className="nav">
-                  <li>
-                    <a href="/" className="active">
-                      Home
-                    </a>
-                  </li>
-                  <li>
-                    <a href="/cart">Cart</a>
-                  </li>
-                  <li>
-                    <a href="/subscription">Subscription</a>
-                  </li>
-                  <li>
-                    <a href="/login">Login</a>
-                  </li>
-                  <li>
-                    <a href="/profile">
-                      Profile <img src={profileHeaderImg} alt="" />
-                    </a>
-                  </li>
-                </ul>
-                <a className="menu-trigger">
-                  <span>Menu</span>
-                </a>
-              </nav>
-            </div>
+  const handleLogout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    window.location.href = "/login";
+  };
+
+  return (
+    <header className="header-area header-sticky">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <nav className="main-nav">
+              <a href="/" className="logo">
+                <h4>
+                  <em>Manga</em> Hub
+                </h4>
+              </a>
+              <div className="search-input"></div>
+              <ul className="nav">
+                <li><a href="/">Home</a></li>
+                <li><a href="/cart">Cart</a></li>
+                <li><a href="/subscription">Subscription</a></li>
+                <li>
+                  <a href="/profile">
+                    Profile <img src={profileHeaderImg} alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#" onClick={(e) => { e.preventDefault(); handleLogout(); }} className="logout-link">
+                    Logout
+                  </a>
+                </li>
+              </ul>
+              <a className="menu-trigger">
+                <span>Menu</span>
+              </a>
+            </nav>
           </div>
         </div>
-      </header>
-    );
-  }
+      </div>
+    </header>
+  );
+}
+
 export default NavBar;
